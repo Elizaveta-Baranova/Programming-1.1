a6dcb9183d380289d23d36de45943436cfa819aa
Liza <elizaveta.b.elizaveta@yandex.by>
2022-08-29 19:42:38 +0300

    задание 4

---
+++ задание4/Program.cs
@@ -0,0 +1,9 @@
+﻿Console.Write("Введите положительное целое число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+int x = 1;
+while (x <= Number)
+{
+    if (x % 2 == 0)
+    Console.Write(x + " ");
+    x = x + 1;
+}
