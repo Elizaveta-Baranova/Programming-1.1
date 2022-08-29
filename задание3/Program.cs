955fdb770de904ab13d55dd0cfd226963d96a6a7
Liza <elizaveta.b.elizaveta@yandex.by>
2022-08-29 19:39:29 +0300

    задание 3

---
+++ задание3/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.Write("Введите число ");
+int Number = Convert.ToInt32(Console.ReadLine());
+if (Number % 2 == 0)
+{
+    Console.Write("Введенное число " + Number + " является четным");
+}
+else
+{
+    Console.Write("Введенное число " + Number + " является нечетным");
+}
